572403a690c72ce66652891fe46741cd4d8a89f3
Gefferson Oliveira <gefferson_oliveira@outlook.com>
2022-06-16 06:33:21 -0300

    ADICIONANDO: Cors.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import handleMiddleware from './middlewares/handleMiddleware';
+const cors = require("cors");
 
 
 const app = express();
 const routes = require("./routes");
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 app.use(handleMiddleware);
