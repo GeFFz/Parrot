620ff9ee1091e24a5d8fa6258fdfa91772c29f69
Gefferson Oliveira <gefferson_oliveira@outlook.com>
2022-06-16 15:18:17 -0300

    ADICIONADO: Iniciando testes Posts. CORREÇÕES: middlewares handle agora retorna status correto em AuthorizationError.

--- backend/src/domain/posts/controllers/post.controller.spec.ts
+++ backend/src/domain/posts/controllers/post.controller.spec.ts
@@ -1,23 +1,49 @@
 import supertest from "supertest";
-import app from "../../../";
+import app from "../../..";
+import { PostController } from "./post.controller"
 
-describe('No controller, ao executar função', () => {
+
+describe('No controller, ao executar a função', () => {
+
+    // const reqGeneric = {
+    //     body: {
+    //         "content": "EU NAO AGUENTO MAIS ESSE BARULHO"
+    //     },
+    //     auth: {
+    //         id: 24,
+    //         email: 'pinsta@gmail.com',
+    //         name: 'Postador do Intagram',
+    //         apartment: 302,
+    //         permission: 1
+
+    //     }
+    // };
+    // const expectedRes = {
+    //     "id": 43,
+    //     "content": "O inquilino do 202 faz muito barulho de madrugada!!!",
+    //     "user_id": 24,
+    //     "updatedAt": "2022-06-16T16:59:08.794Z",
+    //     "createdAt": "2022-06-16T16:59:08.794Z"
+    // }
+
+
 
     describe('create', () => {
         // const emailTest = "5512" ;
 
-        test('em caso de sucesso, deve retornar 201', async () => {
+        test('caso sucesso é do tipo function.', () => {
+            expect(typeof PostController.create).toBe('function')
+        });
+
+        test('em caso de sucesso, deve retornar 401.', async () => {
             const response = await supertest(app)
             .post('/posts')
             .send({
-                content: "AHUSuhAHUS"
+                content: "Testando"
             })
     
-            expect(response.status).toBe(201)
+            expect(response.status).toBe(401);
         })
-
         
-
-
     })
 })
